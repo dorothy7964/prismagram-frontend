ce7cac53b08a5e0f3dd1cf05e63721177e9a2ab2
sample <dorothy_007@naver.com>
2019-09-13 23:22:02 +0900

    #4.2 React Router

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { HashRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from '../Styles/GlobalStyles';
 import Theme from "../Styles/Theme";
+import Routes from "./Routes";
 
 export default () => {
   return (
     <ThemeProvider theme={Theme}>
-      <GlobalStyles />
+      <React.Fragment>
+        <GlobalStyles />
+        <Router>
+          <Routes isLoggedIn={false} />
+        </Router>
+      </React.Fragment>
     </ThemeProvider>
   );
 };
