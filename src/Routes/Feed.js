21ef7d2ca80daadb3a360d9fadc04535f8ae03c7
sample <dorothy_007@naver.com>
2019-09-20 17:13:00 +0900

    #7.0 Getting the Feed and Loader Component

--- src/Routes/Feed.js
+++ src/Routes/Feed.js
@@ -1,1 +1,57 @@
-export default () => "Feed";
+import React from "react";
+import { Helmet } from "react-helmet";
+import styled from "styled-components";
+import { gql } from "apollo-boost";
+import { useQuery } from "react-apollo-hooks";
+import Loader from "../Components/Loader";
+
+const FEED_QUERY = gql`
+    {
+        seeFeed {
+            id
+            location
+            caption
+            user {
+                id
+                userName
+                avatar
+            }
+            files {
+                id
+                url
+            }
+            comments {
+                id
+                text
+            }
+            isLiked
+            likeCount
+        }
+    }
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    min-height: 80vh;
+`;
+
+export default () => {
+    const { data, loading } = useQuery(FEED_QUERY);
+    if(data){console.log(data.seeFeed)};
+    return (
+        <Wrapper>
+            <Helmet>
+                <title>Feed | Prismagram</title>
+            </Helmet>
+            {loading && <Loader />}
+            {!loading &&
+              data &&
+              data.seeFeed &&
+              "we have photos"
+             }
+            
+        </Wrapper>
+    );
+};
