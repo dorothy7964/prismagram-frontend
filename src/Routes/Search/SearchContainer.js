7578b882aef6f1bc4d9a92e6e295bb2990194679
sample <dorothy_007@naver.com>
2019-09-27 13:54:06 +0900

    #8.0 Search Screen Queries

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,1 +1,16 @@
-export default () => "Search";
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { useQuery } from "react-apollo-hooks";
+import { SEARCH } from "./SearchQueries";
+import SearchPresenter from "./SearchPresenter";
+
+export default withRouter(({ location: { search } }) => {
+    const term = search.split("=")[1];
+    const { data, loading } = useQuery(SEARCH, {
+        skip: term === undefined,
+        variables: {
+            term
+        }
+    });
+    return <SearchPresenter searchTerm={term} loading={loading} data={data} />
+});
