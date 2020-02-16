0bde9b995f808a0d274bad6e477444440c583d55
sample <dorothy_007@naver.com>
2020-02-17 05:22:11 +0900

    #17.1 Upload Component part - upload Query

---
+++ src/Routes/Upload/UploadContainer.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import { useMutation } from "react-apollo-hooks";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { UPLOAD } from "./UploadQueries"
+import { FEED_QUERY } from "../../SharedQueries";
+import useInput from "../../Hooks/useInput";
+import UploadPresenter from "./UploadPresenter";
+
+export default ({ history }) => {
+    const captionInput = useInput("");
+    const locationInput = useInput("");
+    const [awsFile, setAwsFile] = useState([]);
+    const [blobFile, setBlobFile] = useState([]);
+    const [isFile, setIsFile] = useState({});
+    const [loading, setLoading] = useState(false);
+    const [uploadMutation] = useMutation(UPLOAD, {
+        refetchQueries: () => [{ 
+            query: FEED_QUERY,
+            variables: {
+                pageNumber: 0,
+                items: 4
+            }
+        }]
+    });
+
+    const url = process.env.NODE_ENV === "development"
+        ? "http://localhost:4000"
+        : "https://prisma-gram-backend.herokuapp.com"
+
+    const handleChange = async (e) => {
+        if(blobFile.length >= 5){
+            return toast.error("사진 5장 까지 업로드 가능합니다.");
+        }
+
+        const files = e.target.files;
+        setIsFile(files);
+        for(var value of files){
+            var obj = URL.createObjectURL(value);
+            blobFile.push(obj);
+            awsFile.push(value);
+        }
+    }
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (captionInput.value === "") {
+            return toast.error("caption을 작성 해주세요.");
+        } else if (awsFile.length === 0){
+            return toast.error("파일 선택 해주세요.");
+        }
+
+        const formData = new FormData();
+        for(var result of awsFile){
+            formData.append("file", result);
+        }
+
+        try {
+            setLoading(true);
+            const {
+                data: { location }
+            } = await axios.post(`${url}/api/uploads`, formData, {
+                headers: {
+                    "content-type": "multipart/form-data",
+                    "Access-Control-Allow-Origin": "*"
+                }
+            });
+            
+            const {
+                data: { upload }
+            } = await uploadMutation({
+                variables: {
+                    files: location,
+                    caption: captionInput.value,
+                    location: locationInput.value
+                }
+            });
+            if(upload){
+                toast.success("업로드 되었습니다.")
+                history.push(`/`);
+            }
+        } catch(e) {
+            toast.error("업로드 실패하였습니다.");
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return (
+        <UploadPresenter
+            isFile={isFile}
+            loading={loading}
+            blobFile={blobFile}
+            captionInput={captionInput}
+            locationInput={locationInput}
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+        />
+    )
+};
---
+++ src/Routes/Upload/UploadPresenter.js
@@ -0,0 +1,98 @@
+import React from "react";
+import styled from "styled-components";
+import Helmet from "react-helmet";
+import Slider from 'react-animated-slider';
+import "./upload.css";
+import Input from "../../Components/Input";
+import Button from "../../Components/Button";
+import Loader from "../../Components/Loader";
+
+const Wrapper = styled.div`
+    min-height: 80vh;
+`;
+
+const UploadForm = styled.form`
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+`;
+
+const UploadImage = styled.div`
+    background-image: url(${props => props.url});
+    background-size: cover;
+    background-repeat: no-repeat;
+    resize: both;
+    margin-bottom: 10px;
+`;
+
+const TextInput = styled(Input)`
+    margin-bottom: 10px;
+`;
+
+const FileSelect = styled.input`
+    margin-bottom: 50px !important;
+`;
+
+const ButtonContainer = styled.span``;
+
+export default ({
+    isFile,
+    loading,
+    blobFile,
+    captionInput,
+    locationInput,
+    handleChange,
+    handleSubmit
+}) => {
+    if(loading === true){
+        return (
+            <Wrapper>
+                <Loader />
+            </Wrapper>
+        );
+    }else if(!loading) {
+        return (
+            <Wrapper>
+                <Helmet>
+                    <title>Uload | Prismagram</title>
+                </Helmet>
+                <UploadForm>
+                    {isFile[0] && (
+                        <Slider>
+                            {blobFile.map((image, index) =>
+                                <UploadImage 
+                                    url={image} 
+                                    key={index}
+                                    onLoad={(e) => URL.revokeObjectURL(image)}
+                                />
+                            )}
+                        </Slider>
+                    )}
+                    <TextInput 
+                        placeholder="caption"
+                        value={captionInput.value}
+                        onChangeText={captionInput.onChange}
+                        {...captionInput} 
+                    />
+                    <TextInput 
+                        placeholder="location"
+                        value={locationInput.value}
+                        onChangeText={locationInput.onChange}
+                        {...locationInput} 
+                    />
+                    <FileSelect 
+                        type="file" 
+                        accept="image/*" 
+                        placeholder="업로드 파일"
+                        onChange={handleChange} 
+                        multiple
+                    />
+                    <ButtonContainer onClick={handleSubmit}>
+                        <Button text={"게시물 등록"} />
+                    </ButtonContainer>
+                </UploadForm>
+            </Wrapper>
+        );
+    }
+};
